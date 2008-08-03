e8316a271bf1d8e64e71b4d11d136c62e0a0d02a
Andy Pearson <andypearson@criticalwebdesign.co.uk>
2008-08-03 22:51:17 +0100

    Updating feed caching to use hpricot

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,35 +1,72 @@
 class Feed < ActiveRecord::Base
   
-  require 'rexml/document'
+  require 'hpricot'
+  require 'open-uri'
+  # require 'rexml/document'
   
   validates_presence_of :name, :url
   
   has_permalink :name, :permalink
   has_many :items
   named_scope :active, :conditions => { :active => true }
-  
-  def xml_source
-    @xml_source ||= REXML::Document.new Net::HTTP.get(URI.parse(url))
-  end
-  
+    
   def self.cache_all
     feeds = self.active
-    items = []
+    @items = []
     
     for feed in feeds
-      xml = feed.xml_source
-      xml.elements.each '//item' do |xml_data|
+      
+      feed.elements.each do |xml_data|
         item = Item.new
         new_item = item.prepare_and_save(feed, xml_data)
-        if new_item
-          items << new_item
-        end
+        @items << new_item if new_item
       end
+      
       feed.cached_at = Time.zone.now
       feed.save!
     end
     
-    items
+    @items
   end
+  
+  def xml_source
+    @xml_source ||= Hpricot.XML(open(url))
+  end
+  
+  def format
+    @format ||= (url.end_with?('.xml')) ? 'XML' : 'RSS'
+  end
+  
+  def elements
+    if (format == 'XML')
+      (xml_source/:track)
+    else
+      (xml_source/:item)
+    end
+  end
+  
+  # def xml_source
+  #   @xml_source ||= REXML::Document.new Net::HTTP.get(URI.parse(url))
+  # end
+  # 
+  # def self.cache_all
+  #   feeds = self.active
+  #   items = []
+  #   
+  #   for feed in feeds
+  #     xml = feed.xml_source
+  #     xml.elements.each '//item' do |xml_data|
+  #       item = Item.new
+  #       new_item = item.prepare_and_save(feed, xml_data)
+  #       if new_item
+  #         items << new_item
+  #       end
+  #     end
+  #     feed.cached_at = Time.zone.now
+  #     feed.save!
+  #   end
+  #   
+  #   items
+  # end
   
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,7 @@ class Item < ActiveRecord::Base
     self.feed_id = feed.id
     self.cached_at = feed.cached_at
     
-    @data = prepare_data(xml_data)
-    format_data(feed.permalink)
+    format_data(xml_data, feed.permalink)
     
     unless cached?
       save!
@@ -24,28 +23,61 @@ class Item < ActiveRecord::Base
     end
   end
   
-  def prepare_data(xml_data)
-    data = {} and xml_data.elements.each do |e|
-      data[e.name.gsub(/^dc:(\\w)/,"\\1").to_s] = e.text || e.attributes['url']
-    end
-    data
-  end
-  
-  def format_data(permalink)
-    self.title = @data['title']
-    self.description = @data['description']
-    self.url = @data['link']
-    self.tags = @data['subject'] || @data['category']
-    
-    published_at = @data['date'] || @data['pubDate']
-    self.published_at = Time.parse(published_at.to_s);
-    
+  def format_data(xml_data, permalink)
+    @data = xml_data
     method_name = "format_#{permalink}_data"
     self.send(method_name) if self.respond_to?(method_name)
   end
   
-  def format_flickr_data
-    puts @data.to_yaml
+  
+  def format_lastfm_data
+    puts @data
   end
+  
+  # def prepare_and_save(feed, xml_data)
+  #     self.feed_id = feed.id
+  #     self.cached_at = feed.cached_at
+  #     
+  #     @data = prepare_data(xml_data)
+  #     format_data(feed.permalink)
+  #     
+  #     unless cached?
+  #       save!
+  #       self
+  #     else
+  #       false
+  #     end
+  #   end
+  # 
+  # def cached?
+  #   if self.feed_id && self.published_at
+  #     item = Item.find(:first, :conditions => { :published_at => self.published_at, :feed_id => self.feed_id })
+  #     !item.blank?
+  #   end
+  # end
+  # 
+  # def prepare_data(xml_data)
+  #   data = {} and xml_data.elements.each do |e|
+  #     data[e.name.gsub(/^dc:(\\w)/,"\\1").to_s] = e.text || e.attributes['url']
+  #   end
+  #   data
+  # end
+  # 
+  # def format_data(permalink)
+  #   self.title = @data['title']
+  #   self.description = @data['description']
+  #   self.url = @data['link']
+  #   self.tags = @data['subject'] || @data['category']
+  #   
+  #   published_at = @data['date'] || @data['pubDate']
+  #   self.published_at = Time.parse(published_at.to_s);
+  #   
+  #   method_name = "format_#{permalink}_data"
+  #   self.send(method_name) if self.respond_to?(method_name)
+  # end
+  # 
+  # def format_flickr_data
+  #   puts @data.to_yaml
+  # end
   
 end
