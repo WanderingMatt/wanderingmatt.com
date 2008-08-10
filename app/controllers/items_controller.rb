4a2fa9deb8f6df217533af4dc4ea358d3a8b87f4
Andy Pearson <andypearson@criticalwebdesign.co.uk>
2008-08-10 18:59:41 +0100

    Adding eager loading to home page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   def index
     limit = (params['offset']) ? 10 : 30
     @offset = (params['offset']) ? params['offset'].to_i : 0
-    @items = Item.find(:all, :limit => limit, :order => 'published_at DESC', :offset => @offset)
+    @items = Item.find(:all, :limit => limit, :order => 'published_at DESC', :offset => @offset, :include => [:feed, :image])
   end
   
 end
