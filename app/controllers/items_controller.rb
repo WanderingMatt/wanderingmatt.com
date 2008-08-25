79008fe2c503036b2ab369eb2adc5117155b9923
Andy Pearson <andypearson@criticalwebdesign.co.uk>
2008-08-25 19:52:07 +0100

    Adding unique count to feed items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,7 @@ class ItemsController < ApplicationController
     limit = (params['offset']) ? 10 : 30
     @offset = (params['offset']) ? params['offset'].to_i : 0
     @items = Item.find(:all, :limit => limit, :order => 'published_at DESC', :offset => @offset, :include => [:feed, :image])
+    @total = Item.count - @offset
   end
   
 end
