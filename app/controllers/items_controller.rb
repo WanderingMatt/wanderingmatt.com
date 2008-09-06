ee26e64d3e9c568409722d4eda815a2b37b3d216
Andy Pearson <andypearson@criticalwebdesign.co.uk>
2008-09-06 23:01:07 +0100

    Fixing bug where new javascript route was not using correct offset

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,8 @@ class ItemsController < ApplicationController
   caches_page :index
   
   def index
-    limit = (params['offset']) ? 10 : 30
-    @offset = (params['offset']) ? params['offset'].to_i : 0
+    limit = (params['id']) ? 10 : 30
+    @offset = (params['id']) ? params['id'].to_i : 0
     @items = Item.find(:all, :limit => limit, :order => 'published_at DESC', :offset => @offset, :include => [:feed, :image])
     @total = Item.count - @offset
   end
