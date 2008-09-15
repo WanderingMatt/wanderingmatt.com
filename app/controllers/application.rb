c02e3eabd6edc8de7037debaee861a84a54305b4
Andy Pearson <andypearson@criticalwebdesign.co.uk>
2008-09-15 20:37:55 +0100

    Extracting admin username/password to external config file

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -17,8 +17,10 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate
-    authenticate_or_request_with_http_basic do |name, password|
-      name == 'admin' && password == 'password'
+    if APP_CONFIG['perform_authentication']
+      authenticate_or_request_with_http_basic do |username, password|
+        username == APP_CONFIG['username'] && password == APP_CONFIG['password']
+      end
     end
   end
   
